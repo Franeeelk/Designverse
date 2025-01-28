a8b439a1dae0947e43c4d51de3787b53cd780770
Yutong Liu <18793825691@163.com>
2025-01-28 18:34:25 +0100

    Add files via upload

---
+++ static/跑代码.py
@@ -0,0 +1,10 @@
+
+n= int(input("please pick a whole number:"))
+q=int(n+1)+n
+r=(q+9)/2 - n
+if n==5:
+    print( "I guess that the result is 5!")
+else:
+    print("Wait, I'm not sure if this magic trick is working well..")
+
+
